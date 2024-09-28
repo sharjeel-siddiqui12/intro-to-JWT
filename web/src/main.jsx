b7ba69954e0a189de724bdabaea31acac3354829
sharjeel-siddiqui12 <sharjeelsiddiqui00@gmail.com>
2024-09-29 01:47:51 +0500

    Refactor: Remove unused code and update main.jsx
    This commit removes commented out code and unused files from the project. It also updates the main.jsx file by fixing the indentation and removing unnecessary comments. This improves the codebase by eliminating unnecessary clutter and improving readability.

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -5,16 +5,12 @@ import './index.css'
 
 import { BrowserRouter } from "react-router-dom";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <BrowserRouter>
       <App />
-    </BrowserRouter>
+  </BrowserRouter>
   </StrictMode>,
 )
